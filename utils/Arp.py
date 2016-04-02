8751985dda02190af4107f8f91826b06e90ed23c
eshel <eshelshay.yaron@gmail.com>
2016-04-02 17:08:35 -0400

    finished first version of Arp.py

--- utils/Arp.py
+++ utils/Arp.py
@@ -1,12 +1,19 @@
-#!/usr/bin/python3.4
-
-__author__ = 'eshel'
-
+#!/usr/bin/python3
+import codecs
 import socket
 import struct
 import uuid
-import codecs
 import argparse
+import re
+
+BROADCAST_MAC = b'\xff' * 6
+
+ARP_BROADCAST_MAC = b'\x00' * 6
+
+__author__ = 'eshel'
+
+
+MAC_RE = re.compile('([0-9a-fA-F]{2})[-:.]?([0-9a-fA-F]{2})[-:.]([0-9a-fA-F]{2})[-:.]?([0-9a-fA-F]{2})[-:.]([0-9a-fA-F]{2})[-:.]?([0-9a-fA-F]{2})')
 
 
 def get_local_mac_address():
@@ -17,6 +24,20 @@ def get_local_mac_address():
     int_mac_addr = uuid.getnode()
     bytes_mac_addr = struct.pack('>IH', int_mac_addr >> 8*2, int_mac_addr % 2**16)
     return bytes_mac_addr
+
+
+def ip_str_to_bytes(ip):
+    """
+    """
+
+    return struct.pack('BBBB', *[int(x) for x in ip.split('.')])
+
+
+def mac_str_to_bytes(mac):
+    """
+    """
+
+    return struct.pack('BBBBBB', *[int(x, 16) for x in MAC_RE.findall(mac)[0]])
 
 
 class ArpPacket(object):
@@ -35,19 +56,24 @@ class ArpPacket(object):
         """
 
         :param dst_ip:
+        :type dst_ip: str
         :param dst_mac:
+        :type dst_mac: str
         :param src_ip:
+        :type src_ip: str
         :param src_mac:
+        :type src_mac: str
         :param payload:
+        :type payload: bytes
         :return:
         """
-        self.dst_ip = dst_ip
-        self.dst_mac = dst_mac or b'\x00' * 6
-        self.eth_dst_mac = eth_dst_mac or b'\xff' * 6
-        self.src_ip = src_ip or struct.pack('BBBB',
-                                            *[int(x) for x in socket.gethostbyname(socket.gethostname()).split('.')])
-        self.src_mac = src_mac or get_local_mac_address()
-        self.eth_src_mac = eth_src_mac or get_local_mac_address()
+        self.dst_ip = ip_str_to_bytes(dst_ip)
+        self.dst_mac = mac_str_to_bytes(dst_mac) if dst_mac else ARP_BROADCAST_MAC
+        self.eth_dst_mac = mac_str_to_bytes(eth_dst_mac) if eth_dst_mac else BROADCAST_MAC
+        self.src_ip = ip_str_to_bytes(src_ip) if src_ip else struct.pack('BBBB',
+                                               *[int(x) for x in socket.gethostbyname(socket.gethostname()).split('.')])
+        self.src_mac = mac_str_to_bytes(src_mac) if src_mac else get_local_mac_address()
+        self.eth_src_mac = mac_str_to_bytes(eth_src_mac) if eth_src_mac else get_local_mac_address()
         self.payload = payload or b''
         self.packet = self._construct_packet()
 
@@ -68,9 +94,29 @@ class ArpPacket(object):
         packet += self.src_mac
         packet += self.src_ip
         packet += self.dst_mac
-        packet += struct.pack('BBBB', *[int(x) for x in self.dst_ip.split('.')])
+        packet += self.dst_ip
         packet += self.payload
         return packet
+
+
+def parse_args():
+    """
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--interface', required=True)
+    parser.add_argument('-d', '--arp-dst-ip', required=True)
+    parser.add_argument('-s', '--arp-src-ip')
+    parser.add_argument('--arp-dst-mac')
+    parser.add_argument('--arp-src-mac')
+    parser.add_argument('--dst-mac')
+    parser.add_argument('--src-mac')
+    padding_group = parser.add_mutually_exclusive_group()
+    padding_group.add_argument('-p', '--padding', type=lambda s: bytes(s, 'ASCII'))
+    padding_group.add_argument('-P', '--hex-padding', type=lambda s: codecs.encode(s, 'hex'))
+
+    args = parser.parse_args()
+
+    return args
 
 
 def main():
@@ -78,15 +124,23 @@ def main():
 
     :return:
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dst-ip', required=True)
-    parser.add_argument('-i', '--interface', required=True)
 
-    args = parser.parse_args()
+    cmd_args = parse_args()
 
-    packet = ArpPacket(args.dst_ip)
+    interface = cmd_args.interface
+    dst_ip = cmd_args.arp_dst_ip
+    src_ip = cmd_args.arp_src_ip
+    dst_mac = cmd_args.arp_dst_mac
+    src_mac = cmd_args.arp_src_mac
+    eth_dst_mac = cmd_args.dst_mac
+    eth_src_mac = cmd_args.src_mac
+    padding = cmd_args.padding or cmd_args.hex_padding
+
     sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
-    sock.bind((args.interface, 0))
+    sock.bind((interface, 0))
+
+    packet = ArpPacket(dst_ip, dst_mac, eth_dst_mac, src_ip, src_mac, eth_src_mac, padding)
+
     sock.send(packet.packet)
 
 
